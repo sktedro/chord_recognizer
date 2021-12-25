168d0364292d24d122fcbbdf5933119bb4a85e76
tedro <sktedro@gmail.com>
2021-12-25 18:42:36 +0100

    FFT now working

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const cors = require("cors");
 const mysql = require("mysql");
 const multer = require("multer");
 const nj = require("numjs");
+const fs = require("fs");
+const path = require("path");
+const wavefileParser = require('wavefile-parser');
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -99,6 +102,95 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+function parseSamples(samples){
+  let output = [];
+  for(let i = 0; i < samples.length; i += 2){
+    // b1 is the lower part since samples are in little endian
+    let b1 = samples[i];
+    // b2 is the upper part of the int16
+    let b2 = samples[i + 1];
+
+    let x = ((b2 & 0xFF) << 8) | (b1 & 0xFF);
+
+    // If b2 is a negative number...
+    if(b2 & (1 << 7)){
+      x = 0xFFFF0000 | x;
+    }
+    output.push(x);
+  }
+  return output;
+}
+
+function makeMono(samples, channels){
+  let output = [];
+
+  for(let i = 0; i < samples.length; i += channels){
+    if(channels > 1){
+      // Take the average
+      let avg = 0;
+      for(let j = 0; j < channels; j++){
+        avg += samples[i + j];
+      }
+      output.push(avg / channels);
+    }
+  }
+
+  return output;
+}
+
+function processAudio(dbRecord){
+  // Read the file
+  const file = fs.readFileSync(path.join(__dirname, "./uploads/" + dbRecord.hash.data));
+
+  // Parse the data
+  const wav = new wavefileParser.WaveFileParser();
+  wav.fromBuffer(file);
+
+  // Save the data
+  dbRecord.sampleRate.data = wav.fmt.sampleRate;
+  dbRecord.lenSamples.data = wav.data.chunkSize;
+  dbRecord.lenSeconds.data = wav.data.chunkSize / wav.fmt.sampleRate / wav.fmt.bitsPerSample * 8 / wav.fmt.numChannels;
+  dbRecord.channels.data = wav.fmt.numChannels;
+  dbRecord.bitsPerSample.data = wav.fmt.bitsPerSample;
+
+  // Parse the samples
+  let samples = parseSamples(wav.data.samples);
+  samples = makeMono(samples, wav.fmt.numChannels);
+  samples = samples.map(x => x / 32768);
+
+  // Parse the samples so it can be passed to the fft (which needs a complex
+  // num)
+  fftInput = [];
+  for(sample in samples){
+    fftInput.push([samples[sample], 0]);
+  }
+  fftInput = nj.array(fftInput);
+
+  // Get the frames
+  frames = [];
+  let step = 1024;
+  let width = 2048;
+  let i = 0;
+  while(i + width < fftInput.shape[0]){
+    frames.push(fftInput.slice([i, i + width]));
+    i += step;
+  }
+  
+  // FFT
+  let ffts = [];
+  for(frame in frames){
+    ffts.push(nj.fft(frames[frame]));
+  }
+  // console.log(ffts[0]);
+  // console.log(JSON.stringify(ffts[0]));
+
+
+  // Pass the data to a NN
+  
+
+  // Return the chords
+}
+
 app.post("/api/upload", upload.single("file"), (req, res) => {
   console.log(req.file);
   if(req.file.mimetype !== "audio/wav"){
@@ -115,17 +207,45 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
         data: "",
         type: "VARCHAR(255)"
       },
+      size: {
+        data: "",
+        type: "INT"
+      },
+      sampleRate: {
+        data: "",
+        type: "INT"
+      },
+      lenSamples: {
+        data: "",
+        type: "INT"
+      },
+      lenSeconds: {
+        data: "",
+        type: "DOUBLE"
+      },
+      channels: {
+        data: "",
+        type: "INT"
+      },
+      bitsPerSample: {
+        data: "",
+        type: "INT"
+      },
     };
+
 
     dbRecord.hash.data = req.file.filename;
     dbRecord.filename.data = req.file.originalname;
+    dbRecord.size.data = req.file.size;
 
-    // Create a new table
+    // Calculate the data
+    processAudio(dbRecord);
+
+    // Create a new table and insert all the data
     createNewTable(dbRecord);
-
-
-    // Insert all the data
     insertData(dbRecord);
+
+    console.log(dbRecord);
 
     res.json({ message: "File successfully uploaded", filename: req.file.filename });
   }
